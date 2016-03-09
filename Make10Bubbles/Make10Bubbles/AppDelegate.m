1ac932be600bb3955598f664b7a8e41bea0dfbc7
OGU <OGU@6bc6e906-94e0-fe49-979f-84a70177fdcf>
2016-03-09 18:52:41 +0000

    Facebook Share added for IOS9 Store 1.1 Archive Published to the App Store

--- Make10Bubbles/Make10Bubbles/AppDelegate.m
+++ Make10Bubbles/Make10Bubbles/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 @interface AppDelegate ()
 
@@ -15,6 +16,8 @@
 @implementation AppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[FBSDKApplicationDelegate sharedInstance] application:application
+                             didFinishLaunchingWithOptions:launchOptions];
     [self arrangeNotifications:application];
     return YES;
 }
@@ -31,9 +34,19 @@
 }
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBSDKAppEvents activateApp];
 }
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                          openURL:url
+                                                sourceApplication:sourceApplication
+                                                       annotation:annotation
+            ];
 }
 
 
--- Make10Bubbles/Make10Bubbles/Constants.h
+++ Make10Bubbles/Make10Bubbles/Constants.h
@@ -9,10 +9,19 @@
 #import <Foundation/Foundation.h>
 
 @interface Constants : NSObject
+
+#define IS_IPHONE5 (([[UIScreen mainScreen] bounds].size.height-568)?NO:YES)
+#define IS_OS_5_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 5.0)
+#define IS_OS_6_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0)
+#define IS_OS_7_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7.0)
+#define IS_OS_8_OR_LATER    ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+
 extern NSString *const WebSite;
 extern NSString *const GameName;
 extern NSString *const AppId;
 extern NSString *const ApiAddress;
 extern NSString *const TwitterAddress;
 extern NSString *const FacebookAddress;
+extern NSString *const FacebookAppId;
+extern NSString *const FacebookAppSecret;
 @end
